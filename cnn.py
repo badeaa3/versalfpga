5c8e51be569ad27ffc54d333731d06be90bfa9c6
Anthony Badea <badeaa3@gmail.com>
2023-12-03 12:52:46 -0600

    fixed old error and now have [VAIQ_ERROR][QUANTIZER_TORCH_TORCH_VERSION]: Installed pytorch version is 1.13.1+cu117, not consistent with pytorch version when compiling quantizer (1.13.1)

--- cnn.py
+++ cnn.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python
+# vitis ai imports
+import pytorch_nndct
+from pytorch_nndct.apis import torch_quantizer
+from pytorch_nndct.apis import Inspector
 
 # python 
 import argparse
@@ -10,9 +13,6 @@ import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import pytorch_nndct
-from pytorch_nndct.apis import Inspector
-from pytorch_nndct.apis import torch_quantizer
 
 # Define the CNN architecture
 class SimpleCNN(nn.Module):
@@ -23,18 +23,16 @@ class SimpleCNN(nn.Module):
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(64)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(1+64*6*10, 128)  # Adjust based on your input size # 64 * 3 * 5
+        self.fc1 = nn.Linear(64*6*10, 128)  # Adjust based on your input size # 64 * 3 * 5
         self.fc2 = nn.Linear(128, 3)  # Adjust output size based on your task
 
-    def forward(self, x, c):
+    def forward(self, x):
         # process the image
         x = x.view(-1, 1, 13, 21)  # Add channel dimension for grayscale image
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = self.pool(x)
         x = x.view(x.shape[0], -1) #-1, 64 * 3 * 5)  # Adjust based on your input size
-        # append event variables (class token)
-        x = torch.cat([x,c],-1)
         # feed forward
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
@@ -49,18 +47,19 @@ if __name__ == "__main__":
     # create test inputs
     x = torch.ones(10,13,21)
     y = torch.ones(10,1)
-    
+
     # Inspect the model.
+    input_shape = x[0]
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     inspector = Inspector("DPUCVDX8G_ISA3_C32B6")
     inspector.inspect(model, (input_shape), device=device, image_format='png')
 
     # nndct_macs, nndct_params = summary.model_complexity(model, input, return_flops=False, readable=False, print_model_analysis=True)
-    input_shape = x[0]
-    quant_dir = "./quantdir"
-    device = torch.device('cpu')
+    quant_dir = "./quant_dir"
+    quant_mode = 'calib'
     quantizer = torch_quantizer(quant_mode, model, (input_shape), output_dir = quant_dir, device=device)
     model = quantizer.quant_model
-
+    
     # quick test evaluate
     model.eval()
     with torch.no_grad():
